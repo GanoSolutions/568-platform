90252db0080fe360ed1135890ac410b063750026
Hermann <her.tamilia@stud.uniroma3.it>
2026-07-01 16:19:10 +0200

    Update User/Employee schema, and response json from GET /Users

---
+++ backend/WebAPI/Models/DTO/EmployeeDTO.cs
@@ -0,0 +1,30 @@
+namespace Five68.Models.DTO
+{
+	public class EmployeeDTO
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string FiscalCode { get; set; }
+		public string Phone { get; set; }
+		public string? Color { get; set; }
+		public DateOnly? ContractEnd { get; set; }
+
+		public static EmployeeDTO FromEmployee(Employee emp)
+		{
+			if (emp is null)
+			{
+				return null;
+			}
+
+			return new EmployeeDTO
+			{
+				Name = emp.Name,
+				Surname = emp.Surname,
+				FiscalCode = emp.FiscalCode,
+				Phone = emp.Phone,
+				ContractEnd = emp.ContractEnd,
+				Color = emp.Color,
+			};
+		}
+	}
+}
